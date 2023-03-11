6da41d6d493933e80be1db5b3b65f031ac33acd0
Rudolf Mets <richardericrinne@gmail.com>
2023-03-11 22:48:56 +0200

    Added information about local time

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (lat, lng, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.  
+        It is currently ${body.location.localtime}`
       );
     }
   });
